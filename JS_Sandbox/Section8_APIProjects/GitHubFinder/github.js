1fc669948da837767dec392e9f3991b7de7567dd
Lonnie Lund <llonnie21@gmail.com>
2022-02-25 14:59:40 -0700

    Finished GitHubUserFinder project.

--- JS_Sandbox/Section8_APIProjects/GitHubFinder/github.js
+++ JS_Sandbox/Section8_APIProjects/GitHubFinder/github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = "cc6f1dbf6734097eb9d2";
     this.client_secret = "4c01011558ea8d14fe0c29c7a8d71a9495bbb1f6";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile, // Same as saying profile: profile
+      repos,
     };
   }
 }
--- JS_Sandbox/Section8_APIProjects/GitHubFinder/ui.js
+++ JS_Sandbox/Section8_APIProjects/GitHubFinder/ui.js
@@ -12,10 +12,10 @@ class UI {
             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
-            <span class="badge badge-info">Following: ${user.following}</span>
+            <span class="badge alert-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge alert-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge alert-success">Followers: ${user.followers}</span>
+            <span class="badge alert-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
               <li class="list-group-item">Company: ${user.company}</li>
@@ -29,5 +29,53 @@ class UI {
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
     `;
+  }
+
+  showRepos(repos) {
+    let output = "";
+    repos.forEach((repo) => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="+blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+              <span class="badge alert-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge alert-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge alert-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    document.getElementById("repos").innerHTML = output;
+  }
+
+  showAlert(message, classes) {
+    this.clearAlert();
+
+    const div = document.createElement("div");
+    div.className = classes;
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".search-container");
+    const search = document.querySelector(".search");
+    container.insertBefore(div, search);
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearProfile() {
+    this.profile.innerHTML = "";
   }
 }
